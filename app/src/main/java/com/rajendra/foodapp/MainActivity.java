f8270d9219614c4c8dff8d26b8849be90739bd50
szidieross <szidieross@gmail.com>
2023-06-06 16:12:39 +0300

    initial commit

---
+++ app/src/main/java/com/rajendra/foodapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.rajendra.foodapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rajendra.foodapp.adapter.AsiaFoodAdapter;
+import com.rajendra.foodapp.adapter.PopularFoodAdapter;
+import com.rajendra.foodapp.model.AsiaFood;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView popularRecycler, asiaRecycler;
+    PopularFoodAdapter popularFoodAdapter;
+    AsiaFoodAdapter asiaFoodAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // now here we will add some dummy data to out model class
+
+//        List<PopularFood> popularFoodList = new ArrayList<>();
+//
+//        popularFoodList.add(new PopularFood("Float Cake Vietnam", "$nothing", R.drawable.popularfood1));
+//        popularFoodList.add(new PopularFood("Chiken Drumstick", "$17.05", R.drawable.popularfood2));
+//        popularFoodList.add(new PopularFood("Fish Tikka Stick", "$25.05", R.drawable.popularfood3));
+//        popularFoodList.add(new PopularFood("Float Cake Vietnam", "$7.05", R.drawable.popularfood1));
+//        popularFoodList.add(new PopularFood("Chiken Drumstick", "$17.05", R.drawable.popularfood2));
+//        popularFoodList.add(new PopularFood("Fish Tikka Stick", "$25.05", R.drawable.popularfood3));
+//
+//        setPopularRecycler(popularFoodList);
+
+
+        List<AsiaFood> asiaFoodList = new ArrayList<>();
+        asiaFoodList.add(new AsiaFood("Chicago Pizza", "$20", R.drawable.p1, "4.8", "Briand Restaurant"));
+        asiaFoodList.add(new AsiaFood("Straberry Cake", "$25", R.drawable.p2, "4.1", "Friends Restaurant"));
+        asiaFoodList.add(new AsiaFood("Chicago Pizza", "$20", R.drawable.p3, "4.5", "Briand Restaurant"));
+        asiaFoodList.add(new AsiaFood("Straberry Cake", "$25", R.drawable.p4, "4.2", "Friends Restaurant"));
+        asiaFoodList.add(new AsiaFood("Chicago Pizza", "$20", R.drawable.p5, "4.5", "Briand Restaurant"));
+        asiaFoodList.add(new AsiaFood("Straberry Cake", "$25", R.drawable.p6, "4.2", "Friends Restaurant"));
+        asiaFoodList.add(new AsiaFood("Straberry Cake", "$25", R.drawable.p7, "4.1", "Friends Restaurant"));
+        asiaFoodList.add(new AsiaFood("Chicago Pizza", "$20", R.drawable.p8, "4.5", "Briand Restaurant"));
+        asiaFoodList.add(new AsiaFood("Straberry Cake", "$25", R.drawable.p9, "4.2", "Friends Restaurant"));
+        asiaFoodList.add(new AsiaFood("Chicago Pizza", "$20", R.drawable.p10, "4.5", "Briand Restaurant"));
+
+        setAsiaRecycler(asiaFoodList);
+
+    }
+
+
+//    private void setPopularRecycler(List<PopularFood> popularFoodList) {
+//
+//        popularRecycler = findViewById(R.id.popular_recycler);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+//        popularRecycler.setLayoutManager(layoutManager);
+//        popularFoodAdapter = new PopularFoodAdapter(this, popularFoodList);
+//        popularRecycler.setAdapter(popularFoodAdapter);
+//
+//    }
+
+    private void setAsiaRecycler(List<AsiaFood> asiaFoodList) {
+
+        asiaRecycler = findViewById(R.id.asia_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        asiaRecycler.setLayoutManager(layoutManager);
+        asiaFoodAdapter = new AsiaFoodAdapter(this, asiaFoodList);
+        asiaRecycler.setAdapter(asiaFoodAdapter);
+
+    }
+}
