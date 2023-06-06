91fc87a401642353b858eaac3722521b52774052
szidieross <szidieross@gmail.com>
2023-06-06 18:25:14 +0300

    style changed

--- app/src/main/java/com/rajendra/foodapp/DetailsActivity.java
+++ app/src/main/java/com/rajendra/foodapp/DetailsActivity.java
@@ -11,4 +11,38 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
     }
+
+//    String name;
+//    String price;
+//    Integer imageUrl;
+//
+//    public DetailsActivity(String name, String price, Integer imageUrl) {
+//        this.name = name;
+//        this.price = price;
+//        this.imageUrl = imageUrl;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
+//
+//    public Integer getImageUrl() {
+//        return imageUrl;
+//    }
+//
+//    public void setImageUrl(Integer imageUrl) {
+//        this.imageUrl = imageUrl;
+//    }
 }
