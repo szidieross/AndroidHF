d399fc5139acf4a24ee7e24e5dbaac23d80275e2
szidieross <szidieross@gmail.com>
2023-06-08 14:02:48 +0300

    added function to orderButton

--- app/src/main/java/com/rajendra/foodapp/CartActivity.java
+++ app/src/main/java/com/rajendra/foodapp/CartActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CartActivity extends AppCompatActivity {
-    private Button buttonGoBack;
+    private Button buttonGoBack, buttonOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +17,19 @@ public class CartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
 
         buttonGoBack = findViewById(R.id.buttonGoBack);
+        buttonOrder = findViewById(R.id.buttonOrder);
 
         buttonGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navigateToMainActivity();
+            }
+        });
+
+        buttonOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeOrder();
             }
         });
     }
@@ -29,5 +38,10 @@ public class CartActivity extends AppCompatActivity {
         Intent intent = new Intent(CartActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void placeOrder() {
+        String message = "Your order has been placed!";
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
