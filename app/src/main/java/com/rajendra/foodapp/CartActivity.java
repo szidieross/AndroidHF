49ac91df4b0aafec80b03f3d27a2fba0450295f4
szidieross <szidieross@gmail.com>
2023-06-08 20:02:02 +0300

    style upgraded for cardActivity

--- app/src/main/java/com/rajendra/foodapp/CartActivity.java
+++ app/src/main/java/com/rajendra/foodapp/CartActivity.java
@@ -1,6 +1,8 @@
 package com.rajendra.foodapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +19,7 @@ public class CartActivity extends AppCompatActivity {
     private LinearLayout cartItemsLayout;
     private double totalPrice = 0.0;
     private int totalAmount = 0;
-    private String price = "0";
+    private String price = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,19 +73,30 @@ public class CartActivity extends AppCompatActivity {
             itemNameTextView.setText(itemName + ": " + itemPrice);
             itemNameTextView.setTextColor(getResources().getColor(R.color.colorText));
             itemNameTextView.setTextSize(18);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                itemNameTextView.setTypeface(Typeface.create(getResources().getFont(R.font.semibold), Typeface.BOLD));
+            }
             cartItemsLayout.addView(itemNameTextView);
         }
 
         TextView totalAmountTextView = new TextView(this);
         totalAmountTextView.setText("Pizza Amount: " + totalAmount);
         totalAmountTextView.setTextColor(getResources().getColor(R.color.colorText));
-        totalAmountTextView.setTextSize(18);
+        totalAmountTextView.setPadding(0, 24, 0, 0);
+        totalAmountTextView.setTextSize(24);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            totalAmountTextView.setTypeface(Typeface.create(getResources().getFont(R.font.semibold), Typeface.BOLD));
+        }
         cartItemsLayout.addView(totalAmountTextView);
 
         TextView totalPriceTextView = new TextView(this);
         totalPriceTextView.setText("Total Price: $" + String.format("%.2f", totalPrice));
         totalPriceTextView.setTextColor(getResources().getColor(R.color.colorText));
-        totalPriceTextView.setTextSize(18);
+        totalPriceTextView.setPadding(0, 24, 0, 0);
+        totalPriceTextView.setTextSize(24);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            totalPriceTextView.setTypeface(Typeface.create(getResources().getFont(R.font.semibold), Typeface.BOLD));
+        }
         cartItemsLayout.addView(totalPriceTextView);
     }
 }
